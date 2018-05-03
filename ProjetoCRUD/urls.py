41cd35bf587697c2358078456616273c9566b163
Leonardo Setubal Maggio <setubal43@gmail.com>
2018-05-03 18:03:26 -0300

    segundo commity

--- ProjetoCRUD/urls.py
+++ ProjetoCRUD/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from app.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('carro/',Cadastrar_Carro,name='Cadastrar_carro')
 ]
--- app/views.py
+++ app/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.forms import ModelForm
+from .models import Carro
 # Create your views here.
+
+class CarroForm(ModelForm):
+    class Meta:
+        model = Carro
+        fields = ['modelo', 'marca', 'ano',
+            'valor', 'data_cadastro']
+
+def Cadastrar_Carro(request):
+    form = CarroForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        return redirect('carro_list')
+    return render(request, 'carro_form',{'form':form})
